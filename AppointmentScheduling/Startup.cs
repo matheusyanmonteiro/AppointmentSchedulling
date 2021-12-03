d4205722754537d147112c22c95494cec2289a54
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2021-12-03 11:27:15 -0300

    create the api appointment endpoint and common response

--- AppointmentScheduling/Startup.cs
+++ AppointmentScheduling/Startup.cs
@@ -32,6 +32,9 @@ namespace AppointmentScheduling
 
       services.AddTransient<IAppointmentService, AppointmentService>();
       services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+      services.AddHttpContextAccessor();
+
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
